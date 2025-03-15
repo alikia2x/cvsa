93bdddc21ee1e8db5f9f227bc0d30d339598c8ec
alikia2x <alikia2x@outlook.com>
2025-03-15 16:51:07 +0800

    fix: unexpectedly commented code to upsert jobs

--- lib/mq/init.ts
+++ lib/mq/init.ts
@@ -3,18 +3,18 @@ import { ClassifyVideoQueue, LatestVideosQueue, SnapshotQueue } from "lib/mq/ind
 import logger from "lib/log/logger.ts";
 
 export async function initMQ() {
-	// await LatestVideosQueue.upsertJobScheduler("getLatestVideos", {
-	// 	every: 1 * MINUTE,
-	// 	immediately: true,
-	// });
-	// await ClassifyVideoQueue.upsertJobScheduler("classifyVideos", {
-	// 	every: 5 * MINUTE,
-	// 	immediately: true,
-	// });
-	// await LatestVideosQueue.upsertJobScheduler("collectSongs", {
-	// 	every: 3 * MINUTE,
-	// 	immediately: true,
-	// });
+	await LatestVideosQueue.upsertJobScheduler("getLatestVideos", {
+		every: 1 * MINUTE,
+		immediately: true,
+	});
+	await ClassifyVideoQueue.upsertJobScheduler("classifyVideos", {
+		every: 5 * MINUTE,
+		immediately: true,
+	});
+	await LatestVideosQueue.upsertJobScheduler("collectSongs", {
+		every: 3 * MINUTE,
+		immediately: true,
+	});
 	await SnapshotQueue.upsertJobScheduler("scheduleSnapshotTick", {
 		every: 3 * MINUTE,
 		immediately: true,
