559c63b43410cf0074c1518e767e0ff35e0531f5
alikia2x <alikia2x@outlook.com>
2025-03-22 00:42:37 +0800

    update: more beautiful time interval formatting

--- lib/utils/formatSeconds.ts
+++ lib/utils/formatSeconds.ts
@@ -3,7 +3,7 @@ export const formatSeconds = (seconds: number) => {
 		return `${(seconds).toFixed(1)}s`;
 	}
 	if (seconds < 3600) {
-		return `${Math.floor(seconds / 60)}m${seconds % 60}s`;
+		return `${Math.floor(seconds / 60)}m${(seconds % 60).toFixed(1)}s`;
 	}
 	return `${Math.floor(seconds / 3600)}h ${((seconds % 3600) / 60).toFixed(2)}m`;
 };
