f585b49ee4dcce193ae884bba58d6cb54e3b7eb3
alikia2x <alikia2x@outlook.com>
2025-03-30 03:57:21 +0800

    add: frontend with Astro

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
+	integrations: [tailwind()],
 	vite: {
 		server: {
 			fs: {
 				allow: [".", "../../"],
 			},
 		},
+		plugins: [tsconfigPaths(),]
 	},
 });
