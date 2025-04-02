d808f36c58543daa444d6fd36b139557217000c6
alikia2x <alikia2x@outlook.com>
2025-04-02 23:32:26 +0800

    improve: number display in benchmark result

--- packages/frontend/src/components/VDFtester.svelte
+++ packages/frontend/src/components/VDFtester.svelte
@@ -191,7 +191,7 @@
                             >{result.N.toString(2).length}</td
                         >
                         <td class="px-6 py-4 font-medium text-zinc-900 whitespace-nowrap dark:text-white"
-                            >{Math.log10(Number(result.difficulty))}</td
+                            >{Math.log10(Number(result.difficulty)).toFixed(2)}</td
                         >
                     </tr>
                 {/each}
