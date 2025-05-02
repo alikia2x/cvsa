811a8261b35c1e84ed2b1fcd8074b44d91eb4c61
alikia2x <alikia2x@outlook.com>
2025-05-02 19:48:17 +0800

    fix: BullMQ crashes when not setting Redis config maxRetriesPerRequest to null

--- packages/core/db/redis.ts
+++ packages/core/db/redis.ts
@@ -3,4 +3,8 @@ import { Redis } from "ioredis";
 const host = process.env.REDIS_HOST || "localhost";
 const port = parseInt(process.env.REDIS_PORT) || 6379;
 
-export const redis = new Redis(port, host);
+export const redis = new Redis({
+    port: port,
+    host: host,
+    maxRetriesPerRequest: null,
+});
