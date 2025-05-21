dd70543594a111a38f13a11ff8fc8beb35ac9d10
alikia2x <alikia2x@outlook.com>
2025-05-22 03:15:07 +0800

    init: next for frontend

---
+++ packages/next/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+	return (
+		<>
+			<main className="flex flex-col items-center justify-center h-full flex-grow gap-8 px-4">
+				<h1 className="text-4xl font-medium text-center">正在施工中……</h1>
+				<p>在搜索栏输入BV号或AV号，可以查询目前数据库收集到的信息~</p>
+			</main>
+		</>
+	);
+}
