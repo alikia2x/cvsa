fb38210f94a5efe4938d0f4091e94d220b32e168
alikia2x <alikia2x@outlook.com>
2025-06-16 23:28:52 +0800

    update: Dockerfile

--- packages/next/.source/source.config.mjs
+++
@@ -1,8 +0,0 @@
-// source.config.ts
-import { defineConfig } from "fumadocs-mdx/config";
-var source_config_default = defineConfig({
-  mdxOptions: {}
-});
-export {
-  source_config_default as default
-};
