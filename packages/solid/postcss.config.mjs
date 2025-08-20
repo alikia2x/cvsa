337fc072ff15bc76d68d989663d4b5dd56bec6c9
alikia2x <alikia2x@outlook.com>
2025-08-21 00:50:35 +0800

    update: upgrade the component library

--- packages/solid/postcss.config.mjs
+++ packages/solid/postcss.config.mjs
@@ -1,6 +1,7 @@
 export default {
 	plugins: {
 		"@tailwindcss/postcss": {},
+		// "@csstools/postcss-oklab-function": {},
 		// "@csstools/postcss-bundler": {},
 		// "@csstools/postcss-cascade-layers": {},
 		// "./src/misc/postcss-calc-keyword-polyfill": {}
