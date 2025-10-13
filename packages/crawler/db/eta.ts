efa1a715123288df71bc152640b4a209a2a6c883
alikia2x <alikia2x@outlook.com>
2025-10-13 23:07:13 +0800

    fix: forgot to set updated_at column of the eta table

--- packages/crawler/db/eta.ts
+++ packages/crawler/db/eta.ts
@@ -5,6 +5,6 @@ export async function updateETA(sql: Psql, aid: number, eta: number, speed: numb
 		INSERT INTO eta (aid, eta, speed, current_views)
 		VALUES (${aid}, ${eta}, ${speed}, ${views})
 		ON CONFLICT (aid)
-		DO UPDATE SET eta = ${eta}, speed = ${speed}, current_views = ${views}
+		DO UPDATE SET eta = ${eta}, speed = ${speed}, current_views = ${views}, updated_at = now()
 	`;
 }
