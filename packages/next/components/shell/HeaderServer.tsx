86e1a2622be90fb2fc3fcaf7c011e2fa11881799
alikia2x <alikia2x@outlook.com>
2025-11-17 01:40:52 +0800

    ref: remove packages/next

--- packages/next/components/shell/HeaderServer.tsx
+++
@@ -1,7 +0,0 @@
-import { Header } from "@/components/shell/Header";
-import { getCurrentUser } from "@/lib/userAuth";
-
-export default async function HeaderServer() {
-	const user = await getCurrentUser();
-	return <Header user={user} />;
-}
