7532e0e96112c6d1c1764393fccf4fddf56244fc
alikia2x <alikia2x@outlook.com>
2026-02-24 22:19:30 +0800

    add: a warning in temp frontend

--- packages/temp_frontend/app/routes/home/index.tsx
+++ packages/temp_frontend/app/routes/home/index.tsx
@@ -13,6 +13,11 @@ export default function Home() {
 	const [input, setInput] = useState("");
 	return (
 		<Layout>
+			<div className="bg-red-100 dark:bg-red-900 text-red-800 dark:text-red-200 p-4 mt-8 rounded-lg">
+				<h2 className="text-red-900 dark:text-red-200 text-2xl font-bold mb-2">警告</h2>
+				<p>您当前看到的是档案馆用于测试的临时网站，并非正式版本。</p>
+				<p>目前此网站上的内容、界面和功能可能与正式版本有很大出入，敬请注意。</p>
+			</div>
 			<h2 className="text-2xl font-medium mt-8 mb-4">小工具</h2>
 			<div className="flex max-sm:flex-col sm:items-center justify-between gap-7 mb-8">
 				<div className="flex gap-3">
